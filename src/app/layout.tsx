ecb8afe4d3138374f01d6f223623b934be3b5029
gitbit-hash <brokedeveloperhelp@gmail.com>
2025-10-28 19:09:32 +0200

    Adding Vercel Web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Providers } from './providers'
 import NextTopLoader from 'nextjs-toploader'
 import Header from './components/Header'
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -90,6 +91,7 @@ export default function RootLayout({
             <Header />
             <main className="flex-1">
               {children}
+              <Analytics />
             </main>
             {/* Cart sidebar */}
             <CartSidebar />
